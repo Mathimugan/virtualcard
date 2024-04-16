9e8d4a35606ecb561b84f6fdf198507a117a999a
Mathimugan <68652175+Mathimugan@users.noreply.github.com>
2024-04-16 10:41:57 +0800

    updated app

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -39,42 +39,42 @@ return (
 		<Form.Label>First Name</Form.Label> 
 		<Form.Control type="text" name="fname" {...register("fname", {
              required: "* Firstname is required"
-           })}   value={user.fname} onChange={handleChange}/> 
+           })}   value={user.fname} onChange={handleChange} autoComplete='off'/> 
               <span className='text-danger'>{errors.fname && errors.fname.message}</span>
 		</Form.Group> 
 		<Form.Group> 
 		<Form.Label>Last Name</Form.Label> 
 		<Form.Control type="text" name="lname" {...register("lname", {
              required: "* Lastname is required"
-           })} value={user.lname} onChange={handleChange}/> 
+           })} value={user.lname} onChange={handleChange} autoComplete='off'/> 
            <span className='text-danger'> {errors.lname && errors.lname.message}</span>
 		</Form.Group> 
         <Form.Group> 
 		<Form.Label>Department</Form.Label> 
 		<Form.Control type="text" name="dept"  {...register("dept", {
              required: "* Department is required"
-           })} value={user.dept} onChange={handleChange}/> 
+           })} value={user.dept} onChange={handleChange} autoComplete='off'/> 
                <span className='text-danger'>{errors.dept && errors.dept.message}</span>
 		</Form.Group>
 		<Form.Group> 
 		<Form.Label>Designation</Form.Label> 
 		<Form.Control type="text" name="position" {...register("position", {
              required: "* Designation is required"
-           })} value={user.position} onChange={handleChange}/>
+           })} value={user.position} onChange={handleChange} autoComplete='off'/>
             <span className='text-danger'>  {errors.position && errors.position.message} </span>
 		</Form.Group> 
         <Form.Group> 
 		<Form.Label>Singapore Mobile Number</Form.Label> 
 		<Form.Control type="number" name="mobile_no_1" {...register("mobile_no_1", {
              required:"* Singapore Mobile Number is required"
-           })} value={user.mobile_no_1} onChange={handleChange}/>
+           })} value={user.mobile_no_1} onChange={handleChange} autoComplete='off'/>
               <span className='text-danger'>{errors.mobile_no_1 && errors.mobile_no_1.message}</span>
 		</Form.Group> 
         <Form.Group> 
 		<Form.Label>Malayasia Mobile Number</Form.Label> 
 		<Form.Control type="number" name="mobile_no_2" {...register("mobile_no_2", {
             required:"* Malaysia Mobile Number is required"
-           })} value={user.mobile_no_2} onChange={handleChange}/>
+           })} value={user.mobile_no_2} onChange={handleChange} autoComplete='off'/>
            <span className='text-danger'>{errors.mobile_no_2 && errors.mobile_no_2.message}</span>
 		</Form.Group> 
         <Form.Group> 
@@ -85,14 +85,14 @@ return (
                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                message: "invalid email address"
              }
-           })} value={user.email} onChange={handleChange}/>
+           })} value={user.email} onChange={handleChange} autoComplete='off'/>
            <span className='text-danger'>{errors.email && errors.email.message}</span>
 		</Form.Group>
         <Form.Group> 
 		<Form.Label>Website</Form.Label> 
 		<Form.Control type="text" name="website"  {...register("website", {
              required: "* Website is required"
-           })} value={user.website} onChange={handleChange}/>
+           })} value={user.website} onChange={handleChange} autoComplete='off'/>
         <span className='text-danger'>{errors.website && errors.website.message}</span>
 		</Form.Group>  
         <br/>
